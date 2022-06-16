e590a8e8b0a1f61deecc13be10996296bb279bec
crismoute <cristianmotaramirez92@gmail.com>
2022-06-15 21:16:39 -0500

    Se creo el componente Boton en components y se crearon los botones en el contenedor principal

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import logotipo from './img/logotipo.png';
 function App() {
   return (
     <div className="App">
+      
       <div className='logo-contenedor'>
         <img
           className='logo'
           src={logotipo}
           alt= 'logo de Ritmosss'
           />
+      </div>
+      <div className='contenedor-principal'>
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={}/>
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={}/>  
       </div>
     </div>
   );
