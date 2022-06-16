ebe14f087ab9299f2feace026b8d6e5d0fb84da2
crismoute <cristianmotaramirez92@gmail.com>
2022-06-15 19:31:38 -0500

    Se agrego el logo y se borraron archivos no necesarios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import logotipo from './img/logotipo.png';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='logo-contenedor'>
+        <img
+          className='logo'
+          src={logotipo}
+          alt= 'logo de Ritmosss'
+          />
+      </div>
     </div>
   );
 }
